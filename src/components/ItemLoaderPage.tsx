fe72475d2e3f0acb6fb9a4e45586eaf3c5bd547c
Emiliano Fernandez <emilianohfernandez@gmail.com>
2021-09-18 20:44:13 -0300

    fix margin list loader

--- src/components/ItemLoaderPage.tsx
+++ src/components/ItemLoaderPage.tsx
@@ -2,16 +2,14 @@ import React from "react";
 
 import ItemLoader from "./ItemLoader";
 
-const margin = { marginTop: 120 };
-
 function ItemLoaderPage() {
   return (
-    <div style={margin}>
+    <>
       <ItemLoader />
       <ItemLoader />
       <ItemLoader />
       <ItemLoader />
-    </div>
+    </>
   );
 }
 
